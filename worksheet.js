b57f98a5d6c146c6d6a575a3bd29157ed4ebb1d8
Lizeth <mendezlizeth084@gmail.com>
2022-04-13 08:13:12 -0700

    Completed First Conditional Problem Meals by Creating 3 conditions in Worksheet File

--- worksheet.js
+++ worksheet.js
@@ -11,3 +11,20 @@ let animalInput= prompt('What is your favorite animal?')
 let colorInput=prompt('What is your favorite color?')
 
 console.log(`I've never seen a ${colorInput} ${animalInput}!`)
+
+//Conditionals - Meals
+//Breakfast- Chilaquiles Lunch- Kale Rice Dinner- Posole Verde
+let timeOfDay= 1200
+
+
+if (timeOfDay < 1200) {
+    console.log(myMeal=myBreakfast='Chilaquiles');
+}
+
+if (timeOfDay >= 1200 && timeOfDay <= 1700) {
+    console.log(myMeal=myLunch= 'Kale Rice');
+}
+
+if (timeOfDay > 1700){
+    console.log(myMeal=myDinner='Posole Verde');
+}
