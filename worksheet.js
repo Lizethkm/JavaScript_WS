307986cfa637ab8fa03bded77f705c3355980a7c
Lizeth <mendezlizeth084@gmail.com>
2022-04-13 07:56:49 -0700

    Completed User Input Problem Consisting of the Prompt Key in Worksheet File

--- worksheet.js
+++ worksheet.js
@@ -4,3 +4,10 @@ console.log(dayOfWeek)
 
 var dayOfWeek='Friday'
 console.log(`I can't wait for ${dayOfWeek}`)
+
+//User Input
+let animalInput= prompt('What is your favorite animal?')
+
+let colorInput=prompt('What is your favorite color?')
+
+console.log(`I've never seen a ${colorInput} ${animalInput}!`)
