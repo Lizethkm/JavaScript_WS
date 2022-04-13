4cbaeb51905c739c6665a015b9011ff732f4d8b9
Lizeth <mendezlizeth084@gmail.com>
2022-04-13 08:36:20 -0700

    Completed Second Conditional Problem by Generating a Random Number with 4 Sets of Conditionals

--- worksheet.js
+++ worksheet.js
@@ -28,3 +28,22 @@ if (timeOfDay >= 1200 && timeOfDay <= 1700) {
 if (timeOfDay > 1700){
     console.log(myMeal=myDinner='Posole Verde');
 }
+
+//Random Numbers
+let randomNumber= Math.floor(Math.random() *11);
+
+if (randomNumber>= 0 && randomNumber <= 2){
+    console.log('Beatles')
+}
+
+if (randomNumber>= 3 && randomNumber <= 5){
+    console.log('Stones')
+}
+
+if (randomNumber>= 6 && randomNumber <= 8){
+    console.log('Floyd')
+}
+
+if (randomNumber== 9 || randomNumber== 10){
+    console.log('Hendrix')
+}
